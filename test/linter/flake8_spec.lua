fa03fb479246fe37b0158c85c3fc815739fbc1d2
Teppei Taguchi <tetzng.tt@gmail.com>
2023-09-13 20:43:13 +0900

    feat: add flake8 for python lint

---
+++ test/linter/flake8_spec.lua
@@ -0,0 +1,73 @@
+describe('flake8', function()
+  it('can lint', function()
+    local ft = require('guard.filetype')
+    ft('python'):lint('flake8')
+    require('guard').setup()
+
+    local diagnostics = require('test.linter.helper').test_with('python', {
+      [[import os]],
+      [[]],
+      [[def foo(n):]],
+      [[    if n == 0:]],
+      [[         return  bar]],
+      [[print('too long sentence to be displayed in one line blah blah blah blah blah blah blah blah blah blah blah blah blah')]],
+    })
+    assert.are.same({
+      {
+        bufnr = 3,
+        col = 0,
+        end_col = 0,
+        end_lnum = 0,
+        lnum = 0,
+        message = "'os' imported but unused [401]",
+        namespace = 69,
+        severity = 3,
+        source = 'flake8',
+      },
+      {
+        bufnr = 3,
+        col = 0,
+        end_col = 0,
+        end_lnum = 2,
+        lnum = 2,
+        message = 'expected 2 blank lines, found 1 [302]',
+        namespace = 69,
+        severity = 1,
+        source = 'flake8',
+      },
+      {
+        bufnr = 3,
+        col = 14,
+        end_col = 14,
+        end_lnum = 4,
+        lnum = 4,
+        message = 'multiple spaces after keyword [271]',
+        namespace = 69,
+        severity = 1,
+        source = 'flake8',
+      },
+      {
+        bufnr = 3,
+        col = 16,
+        end_col = 16,
+        end_lnum = 4,
+        lnum = 4,
+        message = "undefined name 'bar' [821]",
+        namespace = 69,
+        severity = 3,
+        source = 'flake8',
+      },
+      {
+        bufnr = 3,
+        col = 19,
+        end_col = 19,
+        end_lnum = 4,
+        lnum = 4,
+        message = 'trailing whitespace [291]',
+        namespace = 69,
+        severity = 2,
+        source = 'flake8',
+      },
+    }, diagnostics)
+  end)
+end)
